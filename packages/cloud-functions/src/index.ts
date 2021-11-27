b60fa98088ae51538236a7434f7df94b6b34f939
Nazim <nazimali@gmail.com>
2021-11-27 09:58:39 -0500

    MNT: Refactor package configs

--- packages/cloud-functions/src/index.ts
+++ packages/cloud-functions/src/index.ts
@@ -1,3 +1,10 @@
 const doIt = () => "meow";
+const doItYes = (importit = false) => {
+  if (importit) {
+    import("./liba").then((module) => {
+      return doIt() + module.sayYes();
+    });
+  }
+};
 
-export {doIt}
+export { doIt, doItYes };
